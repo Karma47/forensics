43c0fdb8586add75328311780d43255b37136717
divine <prasannavbalaji@gmail.com>
2019-12-09 12:24:18 +0530

    Add files via upload

---
+++ close-case.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Simple script to start shut down listeners. 
+# Intended to be used as part of initial live response.
+# by Dr. Phil Polstra (@ppolstra) as developed for
+# PentesterAcademy.com.
+
+echo "Shutting down listeners at $(date) at user request" | nc localhost 4444
+killall start-case.sh
+killall start-file-listener.sh
+killall nc
+
